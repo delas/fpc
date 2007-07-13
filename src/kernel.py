bc4ba7dfaa96dbcc6f4183339d8d896d39e6c150
Andrea Burattin <delas@users.noreply.github.com>
2007-07-13 16:04:19 +0000

    - Added some methods to kernel module - Added status tray, with full functional start and stop operation and close   button

--- src/kernel.py
+++ src/kernel.py
@@ -3,7 +3,7 @@ import time
 
 # Si connette al database se esiste, se non esiste ne crea uno nuovo
 # con il nome indicato (nello stesso percorso del sorgente pysql.py)
-con = sqlite.connect("../data/archive.db")
+con = sqlite.connect("../data/archive.db", isolation_level = None)
 
 # Crea un cursore che segue la connessione
 cur = con.cursor()
@@ -20,10 +20,9 @@ def getAllProjects():
 
 def addNewProject(name, worked_mins):
 	cur.execute(
-		"INSERT INTO projects (name, worked_mins) VALUES (?, ?)",
+		"INSERT INTO projects (name, worked_mins, work_start_timestamp) VALUES (?, ?, 0)",
 		(name, worked_mins)
 	)
-	con.commit()
 	cur.execute("SELECT id FROM projects ORDER BY id DESC LIMIT 0, 1")
 	last_insert_id = cur.fetchone()[0]
 	addLog(last_insert_id, 0)
@@ -32,6 +31,30 @@ def addNewProject(name, worked_mins):
 
 def getHeaderProjectTable():
 	return ("ID", "Name", "Worked mins")
+
+
+def toggleWorkOnProjectID(project_id):
+	cur.execute("SELECT work_start_timestamp FROM projects WHERE id = ?", (project_id,))
+	session_worked_mins = cur.fetchone()[0]
+	if (session_worked_mins == 0):
+		cur.execute(
+			"UPDATE projects SET work_start_timestamp = ?  WHERE id = ?",
+			(time.time(), project_id)
+		)
+		addLog(project_id, 1)
+	else:
+		lavorati = round((time.time()-session_worked_mins)/60)
+		cur.execute(
+			"UPDATE projects SET work_start_timestamp = 0, worked_mins = worked_mins + ? WHERE id = ?",
+			(lavorati, project_id)
+		)
+		addLog(project_id, 2)
+
+
+def finishAllProjects():
+	for project in getAllProjects():
+		if (project[3] != 0):
+			toggleWorkOnProjectID(project[0])
 
 
 # The possible operation type values are:
@@ -45,4 +68,3 @@ def addLog(project_id, operation_type):
 		"INSERT INTO logs (project_id, operation_type, time) VALUES (?, ?, ?)",
 		(project_id, operation_type, time.time())
 	)
-	con.commit()
